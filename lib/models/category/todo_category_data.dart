1841cd28d5bf14cc7ece66b2438ad0bb3d3b1e89
yeonatalie <yeonatalie@yahoo.com.sg>
2020-06-13 11:47:06 +0800

    category linked with backend. But both category & tasks only show on hot-reload

--- lib/models/category/todo_category_data.dart
+++ lib/models/category/todo_category_data.dart
@@ -3,9 +3,43 @@ import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:summit2/models/category/category_box.dart';
 import 'package:summit2/models/task/task_list.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final databaseReference = Firestore.instance;
+final _auth = FirebaseAuth.instance;
+
+void getCategoryData(List<CategoryBox> listCategories) async {
+  List<CategoryBox> _categories = [];
+  List<String> _categoryNames = [];
+  FirebaseUser user = await _auth.currentUser();
+  String email = user.email;
+  databaseReference
+      .collection('user')
+      .document(email)
+      .collection('to do')
+      .snapshots()
+      .listen((event) {
+    event.documentChanges.forEach((element) {
+      if (element.type == DocumentChangeType.added) {
+        var data = element.document.data;
+        print(data);
+        String categoryName = data['category title'];
+        if (!_categoryNames.contains(categoryName)) {
+          _categoryNames.add(categoryName);
+          CategoryBox category = CategoryBox(categoryName, TasksList(categoryName));
+          listCategories.add(category);
+        }
+      }
+    });
+  });
+}
 
 class CategoryData extends ChangeNotifier {
   List<CategoryBox> _categories = [];
+  CategoryData(){
+    getCategoryData(_categories);
+  }
 
   UnmodifiableListView<CategoryBox> get categories {
     return UnmodifiableListView(_categories);
