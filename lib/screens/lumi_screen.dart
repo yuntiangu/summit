7ccc1a5fe9c256aeb43b6159eabb09bf71f81caa
yeonatalie <yeonatalie@yahoo.com.sg>
2020-07-16 00:25:19 +0800

    lumiNUS email stored in Firebase Auth

--- lib/screens/lumi_screen.dart
+++ lib/screens/lumi_screen.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:summit2/screens/todoScreens/todo_home.dart';
-import 'package:http/http.dart';
+import 'package:oauth2/oauth2.dart' as oauth2;
+//import 'package:http/http.dart';
+import 'package:dio/dio.dart';
 
 class LumiScreen extends StatefulWidget {
   static const String id = 'lumi_screen';
@@ -21,6 +25,9 @@ class _LumiScreenState extends State<LumiScreen> {
   final _auth = FirebaseAuth.instance;
   String authorizationCode;
   String redirectUri = 'summit2:/oauth2-redirect';
+  String accessToken;
+  dynamic apiResponse;
+  String lumiEmail;
 
   @override
   void dispose() {
@@ -45,8 +52,8 @@ class _LumiScreenState extends State<LumiScreen> {
 
     _onStateChanged =
         flutterWebviewPlugin.onStateChanged.listen((WebViewStateChanged state) {
-          print("onStateChanged: ${state.type} ${state.url}");
-        });
+      print("onStateChanged: ${state.type} ${state.url}");
+    });
 
     // Add a listener to on url changed
     _onUrlChanged = flutterWebviewPlugin.onUrlChanged.listen((String url) {
@@ -55,17 +62,97 @@ class _LumiScreenState extends State<LumiScreen> {
           print("URL changed: $url");
           print(redirectUri);
           if (url.startsWith(redirectUri)) {
-            authorizationCode = url.split('?code=')[1];
+            this.authorizationCode = url.split('?code=')[1];
             print("authorization code: $authorizationCode");
-            Navigator.pushNamed(context, TodoHome.id);
-            flutterWebviewPlugin.close();
+            getAccessToken(authorizationCode).then((value) {
+              this.accessToken = value;
+              print('access token: $accessToken');
+              callApi().then((value) {
+                this.apiResponse = value;
+                print('api response: $apiResponse');
+                lumiEmail = getLumiEmail();
+                print(lumiEmail);
+                firebaseUser(lumiEmail);
+              });
+            });
+            //Navigator.pushNamed(context, TodoHome.id);
+            //flutterWebviewPlugin.close();
           }
         });
       }
     });
   }
 
+  Future<String> getAccessToken(String authorizationCode) async {
+    var dio = Dio();
+    Response response = await dio.post(
+      'https://luminus.azure-api.net/login/ADFSToken',
+      options: Options(
+        headers: {
+          'Ocp-Apim-Subscription-Key': 'c9672e39d6854ec084706e9a944f8b21',
+        },
+        contentType: 'application/x-www-form-urlencoded',
+        followRedirects: true,
+      ),
+      data: {
+        'grant_type': 'authorization_code',
+        'resource': 'sg_edu_nus_oauth',
+        'client_id': 'INC000002163230',
+        'code': authorizationCode,
+        "redirect_uri": "summit2:/oauth2-redirect"
+      },
+    );
+    String accessToken = response.data['access_token'];
 
+    return accessToken;
+    //print('pls work sighs ${response.data['access_token']}');
+  }
+
+  Future<dynamic> callApi() async {
+    var dio = Dio();
+    Response response = await dio.get(
+      'https://luminus.azure-api.net/module',
+      options: Options(
+        headers: {
+          'Ocp-Apim-Subscription-Key': 'c9672e39d6854ec084706e9a944f8b21',
+          'Authorization': 'Bearer ${this.accessToken}',
+        }
+      ),
+    );
+    return response.data;
+  }
+
+  String getLumiEmail() {
+    final parts = accessToken.split('.');
+    final payload = utf8.decode(base64.decode(base64.normalize(parts[1])));
+    final payloadMap = json.decode(payload);
+    return payloadMap['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'];
+  }
+
+  void firebaseUser(String luminusUser) async {
+    try {
+      print('hmm');
+      final user = await _auth.signInWithEmailAndPassword(email: luminusUser, password: 'luminus_auth');
+      if (user != null) {
+        Navigator.pushNamed(context, TodoHome.id);
+        flutterWebviewPlugin.close();
+      }
+    } catch (e) {
+      print(e);
+      try {
+        final newUser = await _auth.createUserWithEmailAndPassword(
+          email: luminusUser,
+          password: 'luminus_auth',
+        );
+        if (newUser != null) {
+          Navigator.pushNamed(context, TodoHome.id);
+          flutterWebviewPlugin.close();
+        }
+      } catch (e) {
+        print(e);
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
