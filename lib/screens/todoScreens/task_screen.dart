057cfc7ba3c4b88ef094f6ca3c7d7dd35641005e
yeonatalie <yeonatalie@yahoo.com.sg>
2020-07-26 01:19:48 +0800

    edited comments

--- lib/screens/todoScreens/task_screen.dart
+++ lib/screens/todoScreens/task_screen.dart
@@ -2,14 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:summit2/components/add_fab.dart';
 import 'package:summit2/constants.dart';
-import 'package:summit2/models/category/category_box.dart';
-import 'package:summit2/models/category/category_list.dart';
 import 'package:summit2/components/bottom_bar.dart';
 import 'package:summit2/models/task/task_list.dart';
-import 'package:summit2/models/task/task_tile.dart';
-import 'package:summit2/models/task/todo_task.dart';
 import 'package:summit2/models/task/todo_task_data.dart';
-import 'add_category_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'add_task_screen.dart';
@@ -95,52 +90,3 @@ class _TaskScreenState extends State<TaskScreen> {
   }
 }
 
-//import 'package:flutter/material.dart';
-//import 'package:summit2/components/add_fab.dart';
-//import 'package:summit2/constants.dart';
-//import 'package:summit2/models/task/task_list.dart';
-//
-//import 'add_task_screen.dart';
-//
-//class TaskScreen extends StatelessWidget {
-//  String _title;
-//  TaskScreen(this._title);
-//  static const String id = 'todo_task_screen';
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: AppBar(
-//        iconTheme: IconThemeData(
-//          color: kDarkBlueGrey, //change your color here
-//        ),
-//        backgroundColor: Colors.transparent,
-//        elevation: 0.0,
-//        title: Text(
-//          _title,
-//          style: kHeaderTextStyle,
-//        ),
-//      ),
-//      body: Column(
-//        crossAxisAlignment: CrossAxisAlignment.start,
-//        children: <Widget>[
-//          Container(
-//            child: Expanded(
-//              child: Container(
-//                padding: EdgeInsets.symmetric(horizontal: 20.0),
-//                decoration: BoxDecoration(
-//                    color: Colors.white,
-//                    borderRadius: BorderRadius.only(
-//                      topLeft: Radius.circular(20.0),
-//                      topRight: Radius.circular(20.0),
-//                    )),
-//                child: TasksList(_title),
-//              ),
-//            ),
-//          ),
-//        ],
-//      ),
-//      floatingActionButton: AddFab(screen: AddTaskScreen(_title)),
-//    );
-//  }
-//}
